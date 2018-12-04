3923c336b574fa5e83ae8ea130835a77b35042d9
VALLIS-NERIA <genius-littley@foxmail.com>
2018-12-04 18:41:54 +0800

    dynamic index.html

--- azurenode/src/index.tsx
+++ azurenode/src/index.tsx
@@ -2,6 +2,4 @@ import * as React from "react";
 import { MainRouter } from "./Components/MainRouter/MainRouter";
 import * as ReactDOM from "react-dom";
 
-ReactDOM.render(
-    <MainRouter />,
-    document.getElementById("root"));
+ReactDOM.render(<MainRouter />, document.getElementById("root"));
